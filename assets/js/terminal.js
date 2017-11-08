250c95e843893fa4cc290921ee9652757035fef4
bigboateng2011@gmail.com <bigboateng2011@gmail.com>
2017-11-08 23:38:21 +0000

    implement clear functionality

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -12,6 +12,20 @@ class Terminal {
             "pwd", "clear"
         ];
         this.setListeners(terminal);
+        this.newTerminal =
+            "<p>Last login: Tue Nov 7 03:27:48 on ttys000 |           |Theme: <select id=\"themeSelector\">\n" +
+            "        <option value=0>Black</option>\n" +
+            "        <option value=1>White</option>\n" +
+            "    </select></p>\n" +
+            "    <p><span class=\"green\"></span> - you're in boateng's cli! type `help` to get started.</p>\n" +
+            "    <p class=\"hidden\" id=\"hidden\">\n" +
+            "        <span class=\"prompt\">\n" +
+            "          <span class=\"root\">root</span>\n" +
+            "          <span class=\"tick\">❯</span>\n" +
+            "        </span>\n" +
+            "        <span contenteditable=\"true\" id=\"input\"></span>\n" +
+            "    </p>";
+
     }
 
     static init() {
@@ -21,23 +35,17 @@ class Terminal {
 
     setListeners(terminal) {
 
-        terminal.addEventListener('keyup', (event) => {
-            console.log(event.keyCode);
-        });
-
         terminal.addEventListener("keypress", (event) => {
             let key = event.keyCode;
             if (key === this.commandKeys.enter) {
                 // get the commands
                 let prompt = event.target; // What triggered the keypress event
-                let input = prompt.textContent.trim();
+                let input = prompt.textContent.trim().split(" ");
                 let command = input[0];
                 let args = input[1];
-
-                if (command === 'clear') {
-                    // Clear the console TODO
-                }
-                if (command && command in this.commands) {
+                if (command === "clear") {
+                    this.clearTerminal();
+                } else if (command && command in this.commands) {
                     // handle the command
                 } else {
                     this.terminal.innerHTML += 'Error: command not recognized';
@@ -46,6 +54,13 @@ class Terminal {
                 event.preventDefault();
             }
         })
+    }
+
+
+    clearTerminal() {
+        this.terminal.innerHTML = this.newTerminal;
+        let input = document.getElementById("input");
+        input.focus();
     }
 
 
@@ -61,7 +76,6 @@ class Terminal {
          */
         let copy = oldPrompt.parentNode.cloneNode(true);
         oldPrompt.setAttribute('contenteditable', false); // dont make old one editable
-        console.log(copy);
         this.terminal.append(copy);
         copy.querySelector('#input').innerHTML = '';
         copy.querySelector('#input').focus();
