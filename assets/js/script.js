674ce6ffbc5bf52186bef7b7ba5fcd7a6ef0dd96
bigboateng2011@gmail.com <bigboateng2011@gmail.com>
2017-11-08 22:32:02 +0000

    add some functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let terminal = new Terminal();
+    const terminalWindow = document.getElementById('terminal');
+    const terminal = new Terminal(terminalWindow);
 
+
+    $("#themeSelector").on("change", () => {
+        let value = $("#themeSelector").val();
+        if (value === 0) { // Dark
+            $(body).css('background-color', "#30353a");
+        } else {
+            $(body).css('background-color', "#30353a");
+        }
+    });
 
 });
--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -1,32 +1,76 @@
 class Terminal {
 
-    constructor() {
+    constructor(terminal) {
         let input = document.getElementById("input");
         input.focus();
-        this.setListeners();
+        Terminal.init();
+        this.terminal = terminal;
+        this.commandKeys = {
+            enter: 13,
+        };
+        this.commands = [
+            "pwd", "clear"
+        ];
+        this.setListeners(terminal);
     }
 
-    setListeners() {
-        // First element is theme selector
-        let themeSelect = document.getElementById("themeSelector");
-        let mainBody = document.getElementById("body");
-        themeSelect.addEventListener('change', () => {
-            switch (themeSelect.value) {
-                case 0: // White
-                    console.log(themeSelect.value);
-                    mainBody.style.backgroundColor = "#ffffff";
-                    break;
-                case 1: // Dark
-                    console.log(themeSelect.value);
-                    mainBody.style.backgroundColor = "#30353a";
-                    break;
-                default: // Dark
-                    console.log(themeSelect.value);
-                    mainBody.style.backgroundColor = "#30353a";
-                    break;
+    static init() {
+        $(body).css('background-color', "#30353a");
+    }
 
-            }
+
+    setListeners(terminal) {
+
+        terminal.addEventListener('keyup', (event) => {
+            console.log(event.keyCode);
         });
-    };
+
+        terminal.addEventListener("keypress", (event) => {
+            let key = event.keyCode;
+            if (key === this.commandKeys.enter) {
+                // get the commands
+                let prompt = event.target; // What triggered the keypress event
+                let input = prompt.textContent.trim();
+                let command = input[0];
+                let args = input[1];
+
+                if (command === 'clear') {
+                    // Clear the console TODO
+                }
+                if (command && command in this.commands) {
+                    // handle the command
+                } else {
+                    this.terminal.innerHTML += 'Error: command not recognized';
+                    this.resetPrompt(prompt);
+                }
+                event.preventDefault();
+            }
+        })
+    }
+
+
+    resetPrompt(oldPrompt) {
+        /**
+         <p class="hidden">
+         <span class="prompt">
+         <span class="root">root</span>
+         <span class="tick">❯</span>
+         </span>
+         <span contenteditable="true" id="input"></span>
+         </p>
+         */
+        let copy = oldPrompt.parentNode.cloneNode(true);
+        oldPrompt.setAttribute('contenteditable', false); // dont make old one editable
+        console.log(copy);
+        this.terminal.append(copy);
+        copy.querySelector('#input').innerHTML = '';
+        copy.querySelector('#input').focus();
+    }
+
+}
+
+
+// Deals with the command line interface
+class Cli {
 
 }
